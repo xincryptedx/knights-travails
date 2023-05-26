704f61fb539fd9e3c0a0dc2145ea0f56f98fbed8
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-05-26 19:06:15 -0400

    Add submodule for getting array of possible moves

---
+++ src/getKnightMoves.js
@@ -0,0 +1,6 @@
+// Generate possible moves array
+/* Function that takes knight position as input and returns an array of 
+   all possible valid moves for the knight from that position. Ensure that
+   the moves stay within the bounds of the chessboard. You can use the knight's 
+   movement rules to calculate the new positions. 
+*/
