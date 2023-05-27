01227c2f75d355576112ce7791c0b2bce12f4039
Joshua Barnhart <joshua.r.barnhart@gmail.com>
2023-05-27 17:09:58 -0400

    Add method for comparing array values

--- src/breadthFirstSearch.js
+++ src/breadthFirstSearch.js
@@ -1,3 +1,17 @@
+const arraysContainSameElements = (arr1, arr2) => {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i += 1) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const bfs = (root, propName, targetValue) => {
   // Valiate root
   if (!root || typeof root !== "object") return undefined;
@@ -7,13 +21,13 @@ const bfs = (root, propName, targetValue) => {
   while (q.length > 0) {
     // Get the children of the first object in q
     Object.keys(q[0]).forEach((key) => {
-      if (key !== "position") {
+      if (key !== propName) {
         // If key is for a child node, add it to queue
         q.push(q[0][key]);
       }
     });
     // If the target value is found at the node, return that node
-    if (q[0][targetValue] === targetValue) {
+    if (q[0][propName] === targetValue) {
       return q[0];
     }
     // Remove the first item in the queue
